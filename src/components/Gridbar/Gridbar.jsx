ebf80d2ce6f90c5d863e2a42929efa49b0705047
TobyMike-max <osinubi.michael@gmail.com>
2022-05-23 13:39:15 +0100

    [Modified]: Building grid components

--- src/components/Gridbar/Gridbar.jsx
+++ src/components/Gridbar/Gridbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Gridbar.css'
+import NorthEastIcon from '@mui/icons-material/NorthEast';
+import SouthEastIcon from '@mui/icons-material/SouthEast';
 
 const Gridbar = () => {
   return (
@@ -7,11 +9,22 @@ const Gridbar = () => {
       <div className="balance">
         <div>
           <span className="text">Balance</span>
+          <span className="big_dols">$6,545</span>
+          <NorthEastIcon /> 9%
+        </div>
+        <div>
           <span className="text">Transaction</span>
+          <span className="small_dols">$1,220</span>
         </div>
         <div>
           <span className="text">Income</span>
+          <span className="small_dols">$2,450</span>
+          <NorthEastIcon /> 2%
+        </div>
+        <div>
           <span className="text">Expenses</span>
+          <span className="small_dols">$1,230</span>
+          <SouthEastIcon /> 4.5%
         </div>
       </div>
 
