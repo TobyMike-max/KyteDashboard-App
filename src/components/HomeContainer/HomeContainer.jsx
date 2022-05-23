b1f668764e686bdf0e63c070f5dd265a49e2977a
TobyMike-max <osinubi.michael@gmail.com>
2022-05-23 09:53:09 +0100

    [Modified]: HomeContainer now contains other components

--- src/components/HomeContainer/HomeContainer.jsx
+++ src/components/HomeContainer/HomeContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './HomeContainer.css'
 import Navbar from '../Navbar/Navbar'
+import Gridbar from '../Gridbar/Gridbar'
 
 const HomeContainer = () => {
   return (
     <div className="homeContainer">
       <Navbar />
-      home container
+      <span style={{color:'#FFFFFF', marginTop:'5px', fontWeight:'400'}}>Dashboard</span>
+      <Gridbar />
     </div>
   )
 }
