8a0fb4efd1d317d53496d16162c803b7885f4e5c
TobyMike-max <osinubi.michael@gmail.com>
2022-05-29 20:14:06 +0100

    [Modified]: Added new gridbar components

--- src/components/Gridbar/Gridbar.jsx
+++ src/components/Gridbar/Gridbar.jsx
@@ -51,58 +51,72 @@ const Gridbar = () => {
 
       <div className="myAccounts">
         <span className="text">My Accounts</span>
-        <div className="bar_chart">
-          <div className="bitcoin">
-            Icons
-            Bitcoin
-            $2,636
-            0.068 BTC
+        <div className="charts">
+          <div className="bar_chart">
+            <div className="bitcoin marg">
+              <span>Icons</span>
+              <p>Bitcoin</p>
+              <p className="marg">$2,636</p>
+              <span style={{fontSize:'10px', opacity:'0.8'}}>0.068 BTC</span>
+            </div>
+            <div className="ether marg">
+              <span>Icons</span>
+              <p>Ethereum</p>
+              <p className="marg">$2,509</p>
+              <span style={{fontSize:'10px', opacity:'0.8'}}>1.09 ETH</span>
+            </div>
+            <div className="ripple marg">
+              <span>Icons</span>
+              <p>Ripple</p>
+              <p className="marg">$582</p>
+              <span style={{fontSize:'10px', opacity:'0.8', borderBottom:'0px'}}>694.01 XRP</span>
+            </div>
+            <div className="doge marg">
+              <span>Icons</span>
+              <p>Dogecoin</p>
+              <p className="marg">$818</p>
+              <span style={{fontSize:'10px', opacity:'0.8'}}>2624.73 DOGE</span>
+            </div>
           </div>
-          <div className="ether">
-            Icons
-            Ethereum
-            $2,509
-            1.09 ETH
-          </div>
-          <div className="ripple">
-            Icons
-            Ripple
-            $582
-            694.01 XRP
-          </div>
-          <div className="doge">
-            Icons
-            Dogecoin
-            $818
-            2624.73 DOGE
-          </div>
-        </div>
-        <div className="pie_chart">
-          <div>
-            32%
-            XRP
+          <div className="pie_chart">
+            <div className="circle">
+              <span style={{fontSize:'20px', color:'white', opacity:'0.5'}}>32%</span>
+              <span style={{fontSize:'11px'}}>XRP</span>
+            </div>
           </div>
         </div>
       </div>
 
       <div className="market">
         <span className="text">Market</span>
-        <div>
-          <div>
-            <span>BTC TO USD</span>
+        <div className="market_container">
+          <div className="market_flex">
+            <span style={{fontSize:'8.5px'}}>BTC TO USD</span>
             <span>3885.00</span>
+            <span className="tiny_text" style={{color:'red'}}>
+              <SouthEastIcon style={{fontSize:'10px'}} />23%
+            </span>
           </div>
-          <div>
-            <span>ETH TO USD</span>
+          <div className="market_flex">
+            <span style={{fontSize:'8.5px'}}>ETH TO USD</span>
             <span>2415.75</span>
+            <span className="tiny_text" style={{color:'green'}}>
+              <NorthEastIcon style={{fontSize:'10px'}}/>2.9%
+            </span>
           </div>
-          <div>
-            <span>XRP TO USD</span>
+          <div className="market_flex">
+            <span style={{fontSize:'8.5px'}}>XRP TO USD</span>
             <span>0.83</span>
+            <span className="tiny_text" style={{color:'green'}}>
+              <NorthEastIcon style={{fontSize:'10px'}}/>21%
+            </span>
           </div>
-          <div>
-            <span>DOGE TO USD</span>
+          <div className="market_flex">
+            <span style={{fontSize:'8.5px'}}>DOGE TO USD</span>
             <span>0.31</span>
+            <span className="tiny_text" style={{color:'red'}}>
+              <SouthEastIcon style={{fontSize:'10px'}} />5.5%
+            </span>
           </div>
         </div>
       </div>
@@ -172,40 +186,46 @@ const Gridbar = () => {
 
       <div className="recentActivity">
         <span className="text">Recent Activity</span>
-        <div>
-          <div>
-            <span> Sent</span>
-            <span>$23.30</span>
+        <div className="recent_container">
+          <div className="activ">
+            <div className="flex_column">
+              <span>BTC</span>
+              <span>Sent</span>
+              <span>$23.30</span>
+            </div>
+            <div className="flex_column">
+              <span>Today</span>
+            </div>
           </div>
-          <div>
-            <span>Today</span>
+          <div className="activ">
+            <div className="flex_column">
+              <span>BTC</span>
+              <span>Received</span>
+              <span>$23.50</span>
+            </div>
+            <div className="flex_column">
+              <span>Today</span>
+            </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <span> Sent</span>
-            <span>$23.50</span>
+          <div className="activ">
+            <div className="flex_column">
+              <span>XRP</span>
+              <span>Received</span>
+              <span>$14.00</span>
+            </div>
+            <div className="flex_column">
+              <span>14 Jan 21</span>
+            </div>
           </div>
-          <div>
-            <span>Today</span>
-          </div>
-        </div>
-        <div>
-          <div>
-            <span>Received</span>
-            <span>$14.00</span>
-          </div>
-          <div>
-            <span>14 Jan 21</span>
-          </div>
-        </div>
-        <div>
-          <div>
-            <span>Received</span>
-            <span>$12.25</span>
-          </div>
-          <div>
-            <span></span>
+          <div className="activ">
+            <div className="flex_column">
+              <span>XRP</span>
+              <span>Received</span>
+              <span>$12.25</span>
+            </div>
+            <div className="flex_column">
+              <span></span>
+            </div>
           </div>
         </div>
       </div>
